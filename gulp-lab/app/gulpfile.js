b5392a053e3e1efb5d97f3473c9c9f975fad54bc
Sávio Mendes <saviomd@gmail.com>
2017-09-18 14:14:57 -0300

    TODO 6

--- gulp-lab/app/gulpfile.js
+++ gulp-lab/app/gulpfile.js
@@ -15,8 +15,19 @@ gulp.task('processCSS', function() {
   .pipe(gulp.dest('build'));
 });
 
-// TODO 6.1 - add default tasks
+gulp.task('default', ['serve']);
 
-// TODO 6.2 - watch files
+gulp.task('watch', function() {
+  gulp.watch('styles/*.css', ['processCSS']);
+  gulp.watch('js/*.js', ['minify']);
+});
 
-// TODO 6.3b - run a local server
+gulp.task('serve', ['processCSS', 'minify'], function() {
+  browserSync.init({
+    server: '.',
+    port: 3000
+  });
+  gulp.watch('styles/*.css', ['processCSS']).on('change', browserSync.reload);
+  gulp.watch('js/*.js', ['minify']).on('change', browserSync.reload);
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
